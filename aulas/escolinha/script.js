7d50f4dc68c0ba0a4d79f432ff1fb97851c20a93
Heitor Correa <heitorcorrea001@gmail.com>
2024-08-22 01:11:48 -0300

    commit new navbar fixed

--- aulas/escolinha/script.js
+++ aulas/escolinha/script.js
@@ -1,3 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownItems = document.querySelectorAll('.dropdown__item');
+  
+    dropdownItems.forEach(item => {
+        const link = item.querySelector('.nav__link');
+        link.addEventListener('click', function () {
+            const menu = item.querySelector('.dropdown__menu');
+  
+            // Fecha outros menus que possam estar abertos
+            dropdownItems.forEach(otherItem => {
+                if (otherItem !== item) {
+                    const otherMenu = otherItem.querySelector('.dropdown__menu');
+                    if (otherMenu) {
+                        otherMenu.style.maxHeight = null;
+                    }
+                }
+            });
+  
+            // Alterna o menu do item clicado
+            if (menu.style.maxHeight) {
+                menu.style.maxHeight = null;
+            } else {
+                menu.style.maxHeight = menu.scrollHeight + 'px';
+            }
+        });
+    });
+  
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+  
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('show-menu');
+        this.classList.toggle('show-icon');
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona todos os itens de dropdown
+    const dropdownItems = document.querySelectorAll('.dropdown__item');
+    
+    dropdownItems.forEach(item => {
+      item.addEventListener('click', function() {
+        // Remove a classe 'active' de todos os itens
+        dropdownItems.forEach(el => {
+          if (el !== item) {
+            el.classList.remove('active');
+          }
+        });
+        // Alterna a classe 'active' no item clicado
+        item.classList.toggle('active');
+      });
+    });
+  });
+
+
 let currentSlide = [0, 0, 0];
 
  function showSlide(index, carouselIndex) {
@@ -27,7 +82,3 @@ let currentSlide = [0, 0, 0];
  document.querySelectorAll('.carousel').forEach((carousel, index) => {
      showSlide(0, index);
  });
-
- $('.icon').click(function(){
-    $('span').toggleClass("cancel");
-  });
